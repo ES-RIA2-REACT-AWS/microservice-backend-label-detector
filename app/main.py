f4e40725de234c4f83ab4c5a3cf8d0660cb9ef85
melohan <melodie.ohan@cpnv.ch>
2023-03-21 00:41:23 +0100

    Update main with LabelDetectorController

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from controllers.label_detector_controller import router as label_detector_router
 
 app = FastAPI()
+app.include_router(label_detector_router)
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=3000)
