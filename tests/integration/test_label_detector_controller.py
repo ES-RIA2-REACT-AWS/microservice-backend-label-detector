949505b13f9e2270c95911ff72c8b827023e980e
melohan <melodie.ohan@cpnv.ch>
2023-03-21 03:26:30 +0100

    Update test with a real image

--- tests/integration/test_label_detector_controller.py
+++ tests/integration/test_label_detector_controller.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from fastapi.testclient import TestClient
 from main import app
 from models.label_detector_model import LabelDetectorModel
@@ -26,6 +27,14 @@ class TestLabelDetectorController(unittest.TestCase):
 
     def test_analyze_nominal_case_returns_labels(self):
         # given
+        input_data = LabelDetectorModel(
+            image_url="https://i.natgeofe.com/n/874df281-d3e0-489a-98c0-6b840023b828/newyork_NationalGeographic_2328428_16x9.jpg",
+            max_label=10,
+            min_confidence_level=0.5
+        )
+
         # when
+        response = self.client.post("/analyze", json=input_data.dict())
+
         # then
-        pass
+        self.assertEqual(response.status_code, 200)
