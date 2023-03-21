fdcd419c90ef0235cec2604700f8261c57eed13d
melohan <melodie.ohan@cpnv.ch>
2023-03-21 03:35:56 +0100

    Use services AwsRekognitionService.analyze() method and throw the appropriate exception

--- app/controllers/label_detector_controller.py
+++ app/controllers/label_detector_controller.py
@@ -5,19 +5,22 @@
 # Remarks:   -
 # -----------------------------------------------------------------------------------
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+
 from models.label_detector_model import LabelDetectorModel
+from services.image_analyzer.aws_rekognition_service import AwsRekognitionService
+from services.image_analyzer.errors.aws_rekognition_error import AwsRekognitionError
 
 router = APIRouter()
 
 
 @router.post("/analyze")
 async def analyze(input_data: LabelDetectorModel) -> dict:
-    return {
-        "Labels": [
-            {
-                "Name": "Animal",
-                "Confidence": 0.9
-            }
-        ]
-    }
+    try:
+        rekognition = AwsRekognitionService()
+        results = await rekognition.analyze(input_data.image_url, input_data.max_label, input_data.min_confidence_level)
+        return results
+    except ConnectionError:
+        raise HTTPException(status_code=500)
+    except (ValueError, KeyError, TypeError, AwsRekognitionError):
+        raise HTTPException(status_code=422)
